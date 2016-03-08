456773a4a9e9362aaccf6e66a4eb7e32063c3e12
Mike Ulvila <mikeulvila@me.com>
2016-03-08 09:36:38 -0600

    added maxSize

--- stack.js
+++ stack.js
@@ -8,11 +8,17 @@ class Node {
 }
 
 class Stack {
-  constructor () {
+  constructor (size) {
     this.head = null
+    this.maxSize = size
+    this.length = 0
   }
 
   push (item) {
+    if (this.isFull()) {
+      throw new Error('Stack full')
+    }
+
     let node
 
     if (item instanceof Node) {
@@ -23,12 +29,28 @@ class Stack {
 
     node.next = this.head
     this.head = node
+
+    this.length++
   }
 
   pop () {
     const toRemove = this.head
 
     this.head = toRemove.next
+    this.length--
+
     return toRemove
+  }
+
+  isEmpty () {
+    return !this.length
+  }
+
+  isFull () {
+    return this.length >= this.maxSize
+  }
+
+  getSize () {
+    return this.length
   }
 }
